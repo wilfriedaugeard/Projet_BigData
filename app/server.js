10034b3a475468bf1484400543c9dbf322d779d3
wilfriedaugeard <wilfried.augeard@etu.u-bordeaux.fr>
2021-01-14 09:43:25 +0100

    base page hashtag ok

--- app/server.js
+++ app/server.js
@@ -18,6 +18,7 @@ const top_k_lang = require("./routes/top_k_lang.js")
 const top_k_hashtag_simple = require("./routes/top_k_hashtag_simple.js")
 const top_k_hashtag_triplet = require("./routes/top_k_hashtag_triplet.js")
 const user = require("./routes/user.js")
+const hashtag = require("./routes/hashtag.js")
 
 default_path.init(app);
 dashboard.init(app);
@@ -26,7 +27,7 @@ top_k_lang.init(app)
 top_k_hashtag_simple.init(app)
 top_k_hashtag_triplet.init(app)
 user.init(app)
-
+hashtag.init(app)
 
 
 app.listen(port, function () {
