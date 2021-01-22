af2226b9e5334b9240c85ea93debf01343406d3f
ctarmil <chaima.tarmil@etu.u-bordeaux.fr>
2021-01-22 16:34:39 +0100

    debug

--- data/src/main/java/bigdata/builder/BuilderRDDHashtags.java
+++ data/src/main/java/bigdata/builder/BuilderRDDHashtags.java
@@ -105,9 +105,9 @@ public class BuilderRDDHashtags {
 
         return tripletRDD
                 .reduceByKey((a, b) -> new Tuple2<Long, Set<User>>(a._1 + b._1, a._2.addAll(b._2)))
-                .mapToPair(item -> new Tuple2<Long, Triplet>(item._2, item._1))
+                .mapToPair(item -> new Tuple2<Tuple2<Long,Set<User>>, Triplet>(item._2, item._1))
                 .sortByKey(new TripletComparator(), false, 1)
-                .mapToPair(item -> new Tuple2<Triplet, Long>(item._2, item._1));
+                .mapToPair(item -> new Tuple2<Triplet, Tuple2<Long,Set<User>>>(item._2, item._1));
 
     }
 }
